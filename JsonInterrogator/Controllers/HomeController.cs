d83f3cf93ac17433391ff9ef1a715e917473b873
Daniel Vo <dan@daniel-vo.com>
2020-07-16 22:04:07 -0500

    Cleaned up boilerplate
    Trying to refocus attention to code I produced over boilerplate

--- JsonInterrogator/Controllers/HomeController.cs
+++ JsonInterrogator/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Mvc;
 using JsonInterrogator.Models;
 using Microsoft.AspNetCore.Hosting;
 using System.IO;
@@ -12,14 +10,11 @@ namespace JsonInterrogator.Controllers
     public class HomeController : Controller
     {
         private readonly IWebHostEnvironment _environment;
-        private readonly ILogger<HomeController> _logger;
-
         private IEnumerable<Person> People;
 
-        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment environment)
+        public HomeController(IWebHostEnvironment environment)
         {
             _environment = environment;
-            _logger = logger;
         }
 
         public IActionResult Index()
@@ -32,17 +27,6 @@ namespace JsonInterrogator.Controllers
             }
             var viewModel = new AppViewModel(People);
             return View(viewModel);
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
